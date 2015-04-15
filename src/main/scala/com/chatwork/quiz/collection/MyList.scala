0e4405e1dddbe7361d94563bb8c546ad500b7b11
Takeshi Kunimi <takeshi.kunimi@gree.net>
2015-04-15 14:29:10 +0900

    update

--- src/main/scala/com/chatwork/quiz/collection/MyList.scala
+++ src/main/scala/com/chatwork/quiz/collection/MyList.scala
@@ -5,27 +5,32 @@ import com.chatwork.quiz.MyOption
 sealed trait MyList[+A] {
 
   // Easy
-  def length: Int = ???
+  def length: Int = foldLeft(0)((s, _) => s + 1)
 
   // Normal
-  def foldLeft[B](z: B)(f: (B, A) => B): B = ???
+  def foldLeft[B](z: B)(f: (B, A) => B): B = this match {
+    case MyCons(h, t) => t.foldLeft(f(z, h))(f)
+    case MyNil => z
+  }
 
   // 難易度選択制
   // Normal: 条件 - 特にありません、気の向くままに実装してください。
   // Hard:   条件 - foldLeftを使って実装してください。
-  def foldRight[B](z: B)(f: (A, B) => B): B = ???
+  def foldRight[B](z: B)(f: (A, B) => B): B = this.reverse.foldLeft(z)((s, x) => f(x, s)) // todo 引数名てきとうすぎる
 
   // Normal
   // scalastyle:off
-  def ::[B >: A](b: B): MyList[B] = ???
+  def ::[B >: A](b: B): MyList[B] = MyCons(b, this)
+
   // scalastyle:on
 
   // Normal
-  def reverse: MyList[A] = ???
+  def reverse: MyList[A] = foldLeft[MyList[A]](MyNil)((s, x) => MyCons(x, s))
 
   // Normal
   // scalastyle:off
-  def ++[B >: A](b: MyList[B]): MyList[B] = ???
+  def ++[B >: A](b: MyList[B]): MyList[B] = foldRight(b)((l, r) => MyCons(l, r))
+
   // scalastyle:on
 
   // Normal
@@ -56,9 +61,12 @@ case class MyCons[+A](head: A, tail: MyList[A]) extends MyList[A]
 object MyList {
 
   // Easy
-  def empty[A]: MyList[A] = ???
+  def empty[A]: MyList[A] = MyNil
 
   // Normal
-  def apply[A](as: A*): MyList[A] = ???
+  def apply[A](as: A*): MyList[A] = as.headOption match {
+    case Some(h) => MyCons(h, apply(as.tail: _*))
+    case _ => MyNil
+  }
 
 }
